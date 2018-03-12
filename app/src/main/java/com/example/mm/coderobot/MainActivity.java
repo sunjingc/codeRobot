5e1bd7b6ea4c84b4989b00a14e1f77e12c89a543
sunjingc <1720663962@qq.com>
2018-03-12 15:04:24 +0800

    remove a bug about the id "fab"
    Signed-off-by: sunjingc <1720663962@qq.com>

--- app/src/main/java/com/example/mm/coderobot/MainActivity.java
+++ app/src/main/java/com/example/mm/coderobot/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.mm.coderobot;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,15 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Button mStartRobotButton = (Button) findViewById(R.id.start_the_robot);
         mStartRobotButton.setOnClickListener(new View.OnClickListener() {
